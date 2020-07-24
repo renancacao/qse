d5219d3f7a86141fe57d07cacc74a73332c77d58
renan <rcacao10@gmail.com>
2020-07-23 22:06:09 -0300

    refactor loginHelper

--- login/src/main/java/com/rcacao/qse/login/utils/LoginHelper.kt
+++ login/src/main/java/com/rcacao/qse/login/utils/LoginHelper.kt
@@ -4,14 +4,12 @@ import android.content.Context
 import android.content.Intent
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.rcacao.qse.login.R
 import javax.inject.Inject
 
 class LoginHelper @Inject constructor() {
 
-    fun getLoginIntent(): Intent {
+    fun getAuthUiIntent(): Intent {
 
         val providers: ArrayList<AuthUI.IdpConfig> = arrayListOf(
             //AuthUI.IdpConfig.GoogleBuilder().build(),
@@ -26,12 +24,9 @@ class LoginHelper @Inject constructor() {
             .build()
     }
 
-    fun getLoginResponse(data: Intent?): IdpResponse? = IdpResponse.fromResultIntent(data)
-
-    fun getUser(): FirebaseUser? = FirebaseAuth.getInstance().currentUser
+    fun getAuthUiResponse(data: Intent?): IdpResponse? = IdpResponse.fromResultIntent(data)
 
     fun logout(context: Context) {
         AuthUI.getInstance().signOut(context)
     }
-
 }
