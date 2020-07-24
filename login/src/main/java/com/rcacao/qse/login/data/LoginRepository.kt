0b9d6c646c3f5f82d6fd19545b412901e22df548
renan <rcacao10@gmail.com>
2020-07-23 22:04:45 -0300

    extrai interface para login repository

--- login/src/main/java/com/rcacao/qse/login/data/LoginRepository.kt
+++ login/src/main/java/com/rcacao/qse/login/data/LoginRepository.kt
@@ -1,16 +1,8 @@
 package com.rcacao.qse.login.data
 
-import com.google.firebase.auth.FirebaseAuth
-import com.rcacao.qse.core.UserNotFoundException
 import com.rcacao.qse.core.data.DataResult
 import com.rcacao.qse.login.data.model.User
 
-class LoginRepository {
-
-    fun getUser(): DataResult<User> {
-        FirebaseAuth.getInstance().currentUser?.apply {
-            return DataResult.Success(User(this.providerId, this.phoneNumber))
-        }
-        return DataResult.Error(UserNotFoundException())
-    }
+interface LoginRepository {
+    fun getUser(): DataResult<User>
 }
