4eee1bd420015adbda2317a17c276938171f25f9
renan <rcacao10@gmail.com>
2020-07-25 03:02:33 -0300

    Cria activity main que será responsável pela navegação de todo o app

---
+++ app/src/main/java/com/rcacao/qse/main/view/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rcacao.qse.main.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rcacao.qse.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
